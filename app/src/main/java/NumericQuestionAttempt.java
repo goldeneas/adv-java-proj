602e4bec9616ffbd1212a4afc70447fb6017d1cc
15M-I <matteoiandiorio15@gmail.com>
2026-04-28 14:58:17 +0200

    ClasseNumericQuestionAttempt

---
+++ app/src/main/java/NumericQuestionAttempt.java
@@ -0,0 +1,29 @@
+package org.example;
+public class NumericQuestionAttempt {
+    private NumericQuestion question;
+    private int givenAnswer;
+
+    public NumericQuestionAttempt(NumericQuestion question, int givenAnswer) {
+        this.question = question;
+        this.givenAnswer = givenAnswer;
+    }
+
+
+    public NumericQuestion getQuestion() {
+        return question;
+    }
+
+    public int getGivenAnswer() {
+        return givenAnswer;
+    }
+
+    public boolean isCorrect() {
+        return givenAnswer == question.getResult();
+    }
+
+    public String getResult() {
+        return isCorrect() ? "Correct" : "Wrong";
+    }
+
+    
+}
