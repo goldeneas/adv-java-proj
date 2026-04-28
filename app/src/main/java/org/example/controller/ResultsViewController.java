20b0657f875eb1731e130556b727a981f9816516
Nick0471 <nicolomassimolisena@gmail.com>
2026-04-28 15:28:59 +0200

    Aggiunta ResultViewController

---
+++ app/src/main/java/org/example/controller/ResultsViewController.java
@@ -0,0 +1,19 @@
+package main.java.org.example.controller;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.io.IOException;
+
+public class ResultsViewController {
+    @FXML
+    private Button exportBtn;
+
+    private void exportCSV(){
+        try(){
+
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}
